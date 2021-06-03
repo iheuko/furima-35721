4c9586ddf065296771eb3f05095588a39a6532c9
iheuko <iheukosan1@gmail.com>
2021-06-03 22:14:20 +0900

    Rspecによるテストコードを記述

---
+++ spec/factories/users.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  gimei = Gimei.name
+
+  factory :user do
+    
+    nickname {Faker::Name.last_name}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+    birthday{Faker::Date.birthday(min_age: 15, max_age: 18)}
+    last_name{gimei.last.kanji}
+    first_name{gimei.first.kanji}
+    last_name_katakana{gimei.last.katakana}
+    first_name_katakana{gimei.first.katakana}
+  end
+end
