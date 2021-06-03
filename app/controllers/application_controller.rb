80cb20668804cbd42c4767fb2b118d859b349a19
iheuko <iheukosan1@gmail.com>
2021-06-03 15:14:49 +0900

    新規登録フォームの実装（現段階では登録できない）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :authenticate_user!
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :last_name_katakana, :first_name, :first_name_katakana, :nickname, :birthday, :email, :encrypted_password])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  
+  validates :nickname, presence: true
+  validates :birthday, presence: true
+  validates :email, presence: true, uniqueness: true
+      
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+   validates :last_name 
+   validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+   validates :last_name_katakana
+   validates :first_name_katakana
+  end
+
+   with_options presence: true, format: { with: /\A[a-z0-9]+\z/ } do
+    validates :password
+    validates :password_confirmation
+  end  
 end
