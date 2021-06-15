4633130bfb3e2538b3f02dfc01ffe7792f518d5e
iheuko <iheukosan1@gmail.com>
2021-06-15 13:01:28 +0900

    商品購入フォームを作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,9 +6,10 @@ class OrdersController < ApplicationController
 
   def create
     @order_residence = OrderResidence.new(order_params)
-    if @order_residence = valid?
+    if @order_residence.valid?
+      pay_item
       @order_residence.save
-      redirect_to items: :index
+      redirect_to root_path
     else
       @item = Item.find(params[:item_id])
       render :index
@@ -19,6 +20,16 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_residence).permit(:postal_code, :state_id, :city, :address_line, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:id])
+    params.require(:order_residence).permit(:postal_code, :state_id, :city, :address_line, :building_name, :phone_number).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def pay_item
+    item = Item.find(params[:item_id])
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: item.price, 
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy' 
+    )
   end
 end
--- app/models/order_residence.rb
+++ app/models/order_residence.rb
@@ -1,17 +1,16 @@
 class OrderResidence
   include ActiveModel::Model
-  attr_accessor :postal_code, :state_id, :city, :address_line, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :token, :user_id, :item_id, :postal_code, :state_id, :city, :address_line, :building_name, :phone_number
+
+  validates :state_id, numericality: { other_than: 1 }
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
-    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/}
-    validates :address_line, format: { with: /\A[ぁ-んァ-ン一-龥々ー][\d][-]+\z/ }
-    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :phone_number, format: { with: /\A\d{11}\z/ }
+    validates :city
+    validates :address_line
+    validates :token
   end
-
-  validates :state_id, numericality: { other_than: 1 }
-  validates :building_name, format: { with: /\A[ぁ-んァ-ン一-龥々ー][\d][a-z]+\z/i } 
-
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
